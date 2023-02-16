40d31e88957e70471b799abe82693af7b5beb236
Aditya Patel <adipatel1412@gmail.com>
2023-02-16 17:30:10 +0530

    Added DashBoard and BottomNavigationMenu with 4 Fragments

--- app/src/main/java/com/example/tms/DashboardActivity.java
+++ app/src/main/java/com/example/tms/DashboardActivity.java
@@ -1,14 +1,60 @@
 package com.example.tms;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
+import com.example.tms.databinding.ActivityDashboardBinding;
+
 public class DashboardActivity extends AppCompatActivity {
+
+
+    private ActivityDashboardBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dashboard);
+        binding=ActivityDashboardBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+
+            switch (item.getItemId()){
+                case R.id.home:
+
+                    replaceFragment(new HomeFragment());
+                    break;
+                case R.id.announcement:
+
+                    replaceFragment(new AnnouncementFragment());
+                    break;
+                case R.id.academics:
+
+                    replaceFragment(new AcademicsFragment());
+                    break;
+                case R.id.account:
+
+                    replaceFragment(new AccountFragment());
+                    break;
+            }
+
+            return true;
+        });
     }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        fragmentTransaction.setCustomAnimations(R.anim.fade_in, R.anim.fade_in);
+        fragmentTransaction.replace(R.id.frame_layout,fragment);
+        fragmentTransaction.commit();
+
+    }
+
 }
