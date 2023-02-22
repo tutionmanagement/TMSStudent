1eee74f9b8344b7a4400fc9374365f99f00bb03e
Rajvi Patel <rajviptel7102@gmail.com>
2023-02-22 14:54:51 +0530

    Added dropdown to register screen.

--- app/src/main/java/com/example/tms/SignupActivity.java
+++ app/src/main/java/com/example/tms/SignupActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.view.WindowManager;
 import android.widget.CheckBox;
@@ -12,6 +14,7 @@ import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RadioButton;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 public class SignupActivity extends AppCompatActivity {
@@ -21,10 +24,36 @@ public class SignupActivity extends AppCompatActivity {
     private CheckBox cbAgree;
 
     private Button signUp;
+
+    private Spinner spStd;
+    private String[] std = {"-----Select----", "1","2","3","4","5","6","7","8","9","10"};
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
         getSupportActionBar().hide();
+        spStd = findViewById(R.id.spStd);
+
+        ArrayAdapter<String> adapter=new ArrayAdapter<>(getApplicationContext(),
+                android.R.layout.simple_list_item_1,std);
+        spStd.setSelection(1);
+        spStd.setAdapter(adapter);
+        spStd.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                if(i>0)
+                    Toast.makeText(getApplicationContext(), "Your standard is "+std[i], Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                Toast.makeText(getApplicationContext(), "Nothing selected..", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
 
     }
     public void toSignin(View v){
